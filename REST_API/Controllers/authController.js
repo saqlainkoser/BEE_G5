e624495017ec814b616b22f81bfa620f0048efda
saqlainkoser <saqlainkoser@gmail.com>
2024-08-22 15:05:05 +0530

    changes

--- REST_API/Controllers/authController.js
+++ REST_API/Controllers/authController.js
@@ -1,11 +1,16 @@
 //importin model
 const User = require('./../Models/userModel.js')
-
+const jwt = require('jsonwebtoken');
 
 exports.signup = async (req,res,next) =>{
     const newUser = await User.create(req.body);
+
+    const token = jwt.sign({id:newUser._id},process.env.SECRET_STR,{expiresIn:process.env.LOGIN_EXPIRES})
+
+
     res.status(201).json({
         status:'success',
+        token,
         data:{
             user:newUser
         }
